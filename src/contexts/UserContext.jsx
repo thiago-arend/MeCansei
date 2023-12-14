932cb43c2c83888c1efa63489885852d20c5d9a0
thiago-arend <thiago.arend@gmail.com>
2023-12-14 00:00:32 -0300

    fix: changing lsUser variable name to a more semantic one

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,8 +3,8 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
-    const lsUser = localStorage.getItem("user");
-    const [user, setUser] = useState(JSON.parse(lsUser));
+    const userFromLocalStorage = localStorage.getItem("user");
+    const [user, setUser] = useState(JSON.parse(userFromLocalStorage));
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
